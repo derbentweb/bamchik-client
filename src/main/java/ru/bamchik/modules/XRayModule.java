71b6fbf2a05d05ff6798a1bb2b4083272ba763d6
derbentweb <alishkaiahmed@gmail.com>
2026-09-04 16:02:13 +0300

    Update XRayModule.java

--- src/main/java/ru/bamchik/modules/XRayModule.java
+++ src/main/java/ru/bamchik/modules/XRayModule.java
@@ -1,24 +1,27 @@
 package ru.bamchik.modules;
 
-import ru.bamchik.Module;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import java.util.HashSet;
 import java.util.Set;
+import ru.bamchik.Module;
 
 public class XRayModule extends Module {
     public static final Set<Block> ORE_BLOCKS = new HashSet<>();
-    static {
-        ORE_BLOCKS.add(Blocks.IRON_ORE);
-        ORE_BLOCKS.add(Blocks.GOLD_ORE);
+
+    public XRayModule() {
+        super("XRay");
+        
+        // Добавляем актуальные блоки под Minecraft 1.21.1
         ORE_BLOCKS.add(Blocks.DIAMOND_ORE);
-        ORE_BLOCKS.add(Blocks.EMERALD_ORE);
-        ORE_BLOCKS.add(Blocks.REDSTONE_ORE);
-        ORE_BLOCKS.add(Blocks.LAPIS_ORE);
-        ORE_BLOCKS.add(Blocks.COAL_ORE);
+        ORE_BLOCKS.add(Blocks.DEEPSLATE_DIAMOND_ORE);
+        ORE_BLOCKS.add(Blocks.GOLD_ORE);
+        ORE_BLOCKS.add(Blocks.DEEPSLATE_GOLD_ORE);
+        ORE_BLOCKS.add(Blocks.IRON_ORE);
+        ORE_BLOCKS.add(Blocks.DEEPSLATE_IRON_ORE);
         ORE_BLOCKS.add(Blocks.NETHER_QUARTZ_ORE);
-        ORE_BLOCKS.add(Blocks.MOB_SPAWNER);
+        ORE_BLOCKS.add(Blocks.ANCIENT_DEBRIS);
+        ORE_BLOCKS.add(Blocks.SPAWNER); // Исправлено: вместо MOB_SPAWNER теперь просто SPAWNER
+        ORE_BLOCKS.add(Blocks.CHEST);
     }
-    public XRayModule() { super("XRay"); }
-    @Override public void onTick() {}
 }
