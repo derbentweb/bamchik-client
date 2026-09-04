fc2aeb371ddb5e830b3ec680e994e627994939d1
derbentweb <alishkaiahmed@gmail.com>
2026-09-04 22:48:21 +0300

    Add category 'Visuals' to ESPModule constructor

--- src/main/java/ru/bamchik/modules/ESPModule.java
+++ src/main/java/ru/bamchik/modules/ESPModule.java
@@ -4,7 +4,7 @@ import ru.bamchik.Module;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class ESPModule extends Module {
-    public ESPModule() { super("ESP"); }
+    public ESPModule() { super("ESP", "Visuals"); }
     @Override public void onTick() {}
     @Override public void onRender(MatrixStack matrices, float tickDelta) {
         // Рисование рамок через миксин или здесь. Оставим заглушку для простоты.
