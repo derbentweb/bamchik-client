643223ad9bc438a31bdbcc03e594071c349be5fb
derbentweb <alishkaiahmed@gmail.com>
2026-09-04 22:47:12 +0300

    Update AutoClickerModule.java

--- src/main/java/ru/bamchik/modules/AutoClickerModule.java
+++ src/main/java/ru/bamchik/modules/AutoClickerModule.java
@@ -3,7 +3,7 @@ package ru.bamchik.modules;
 import ru.bamchik.Module;
 
 public class AutoClickerModule extends Module {
-    public AutoClickerModule() { super("AutoClicker"); }
+    public AutoClickerModule() { super("AutoClicker", "Combat"); }
     private long last = 0;
     private static final int CPS = 20;
     private static final long DELAY = 1000 / CPS;
