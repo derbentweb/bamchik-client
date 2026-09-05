85ce8bdc4471c46a00005ff28ede1f648c425e25
derbentweb <alishkaiahmed@gmail.com>
2026-09-05 12:01:36 +0300

    Update MixinBlockRenderManager.java

--- src/main/java/ru/bamchik/mixins/MixinBlockRenderManager.java
+++ src/main/java/ru/bamchik/mixins/MixinBlockRenderManager.java
@@ -1,33 +1,35 @@
 package ru.bamchik.mixins;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import ru.bamchik.BamchikClient;
-import ru.bamchik.Module;
-import ru.bamchik.modules.XRayModule;
 
 @Mixin(BlockRenderManager.class)
 public class MixinBlockRenderManager {
-    @Inject(method = "renderBlock", at = @At("HEAD"), cancellable = true)
-    private void onRenderBlock(Block block, BlockPos pos, BlockRenderView world, MatrixStack matrices, CallbackInfo ci) {
-        boolean xrayOn = false;
-        if (BamchikClient.getInstance() != null && BamchikClient.getInstance().isKeyValid()) {
-            for (Module m : BamchikClient.getInstance().getModuleManager().getModules()) {
-                if (m.getName().equals("XRay") && m.isEnabled()) {
-                    xrayOn = true;
-                    break;
-                }
-            }
-        }
-        if (xrayOn && !XRayModule.ORE_BLOCKS.contains(block)) {
-            ci.cancel();
-        }
+
+    @Inject(
+        method = "renderBlock",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void onRenderBlock(
+        BlockState state,
+        BlockPos pos,
+        BlockRenderView world,
+        MatrixStack matrices,
+        VertexConsumer vertexConsumer,
+        boolean cull,
+        Random random,
+        CallbackInfo ci
+    ) {
+        // Логика обработки рендеринга блоков
     }
 }
