1f6e2a4f94cba44d71d9c0c65440a098a34aea3f
derbentweb <alishkaiahmed@gmail.com>
2026-09-05 12:13:58 +0300

    Add ModuleManager to manage game modules

--- src/main/java/ru/bamchik/ModuleManager.java
+++ src/main/java/ru/bamchik/ModuleManager.java
@@ -1,55 +1,122 @@
 package ru.bamchik;
 
-import net.minecraft.client.MinecraftClient;
+import ru.bamchik.modules.*;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 
-public abstract class Module {
-    protected static final MinecraftClient mc = MinecraftClient.getInstance();
+import java.util.ArrayList;
+import java.util.List;
 
-    private final String name;
-    private final String category;
-    private int key;
-    private boolean enabled;
+public class ModuleManager {
+    private final List<Module> modules = new ArrayList<>();
 
-    public Module(String name, String category) {
-        this.name = name;
-        this.category = category;
-        this.key = -1;
-        this.enabled = false;
+    public void initModules() {
+        // Combat
+        add(new KillAuraModule());
+        add(new TriggerBotModule());
+        add(new AutoClickerModule());
+
+        // Movement
+        add(new FlightModule());
+        add(new SpeedModule());
+        add(new NoFallModule());
+
+        // Visuals
+        add(new XRayModule());
+        add(new ESPModule());
+
+        // Misc
+        add(new NukerModule());
+        add(new RandomNickModule());
+        add(new OptimizationModule());
+        add(new BaseFinderModule());
+        add(new SwingAnimationModule());
     }
 
-    public Module(String name, String category, int key) {
-        this.name = name;
-        this.category = category;
-        this.key = key;
-        this.enabled = false;
+    private void add(Module m) {
+        modules.add(m);
     }
 
-    public void toggle() {
-        setEnabled(!enabled);
+    public List<Module> getModules() {
+        return modules;
     }
 
-    public void setEnabled(boolean enabled) {
-        if (this.enabled != enabled) {
-            this.enabled = enabled;
-            if (enabled) {
-                onEnable();
-            } else {
-                onDisable();
+    @SuppressWarnings("unchecked")
+    public <T extends Module> T getModule(Class<T> clazz) {
+        for (Module m : modules) {
+            if (m.getClass() == clazz) {
+                return (T) m;
+            }
+        }
+        return null;
+    }
+
+    public Module getModuleByName(String name) {
+        for (Module m : modules) {
+            if (m.getName().equalsIgnoreCase(name)) return m;
+        }
+        return null;
+    }
+
+    public List<String> getCategories() {
+        List<String> cats = new ArrayList<>();
+        for (Module m : modules) {
+            String cat = m.getCategory();
+            if (!cats.contains(cat)) cats.add(cat);
+        }
+        return cats;
+    }
+
+    public List<Module> getModulesByCategory(String category) {
+        List<Module> list = new ArrayList<>();
+        for (Module m : modules) {
+            if (m.getCategory().equalsIgnoreCase(category)) list.add(m);
+        }
+        return list;
+    }
+
+    public void onKeyPressed(int key) {
+        if (key <= 0) return;
+        for (Module m : modules) {
+            if (m.getKeyBind() == key) {
+                m.toggle();
             }
         }
     }
 
-    public void onEnable() {}
-    public void onDisable() {}
-    public void onTick() {}
-    public void onRender(MatrixStack matrices, float tickDelta) {}
-    public void onHudRender(DrawContext context, float tickDelta) {}
+    public void onTick() {
+        for (Module m : modules) {
+            if (m.isEnabled()) {
+                try {
+                    m.onTick();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-    public String getName() { return name; }
-    public String getCategory() { return category; }
-    public int getKey() { return key; }
-    public void setKey(int key) { this.key = key; }
-    public boolean isEnabled() { return enabled; }
+    public void onRender(MatrixStack matrices, float tickDelta) {
+        for (Module m : modules) {
+            if (m.isEnabled()) {
+                try {
+                    m.onRender(matrices, tickDelta);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void onHudRender(DrawContext context, float tickDelta) {
+        for (Module m : modules) {
+            if (m.isEnabled()) {
+                try {
+                    m.onHudRender(context, tickDelta);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
